a4dbe8ad2503ffbd44f9f8cf07cf184ed5ab3237
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-26 22:49:39 +0900

    fix: fix flake.nix

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        llvm = pkgs.llvmPackages_16;
       in
       {
         devShells.default =
@@ -31,17 +30,10 @@
               openssl
               pkg-config
               bacon
-              llvm.llvm
-              llvm.clang
-              llvm.libclang
+              opencv
+              llvmPackages_20.libcxxClang
               (rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; })
             ];
-            shellHook = ''
-              export LLVM_CONFIG_PATH=${llvm.llvm}/bin/llvm-config
-              export LIBCLANG_PATH=${llvm.libclang.lib}/lib
-              echo "LLVM_CONFIG_PATH=$LLVM_CONFIG_PATH"
-              echo "LIBCLANG_PATH=$LIBCLANG_PATH"
-            '';
             RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
           };
       }
